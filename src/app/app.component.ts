4d0e0d996d880828a7c08cb4ad9fd776c11d3cef
CarlosDevBr <cahzerohenrique@gmail.com>
2022-03-21 11:50:07 -0300

    3.B: NgIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'exemplo02';
+  //Diretiva NGIF
+  exibirTexto:boolean | undefined;
+
+  acaoNgIf(){
+    this.exibirTexto = !this.exibirTexto;
+  }
 }
