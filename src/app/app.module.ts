418c4825e875538f95914761ffcefe3a1e5a59b3
CarlosDevBr <cahzerohenrique@gmail.com>
2022-03-21 11:10:06 -0300

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MeuComponenteComponent } from './meu-componente/meu-componente.component';
+import { MeuComponente2Component } from './meu-componente2/meu-componente2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeuComponenteComponent,
+    MeuComponente2Component
   ],
   imports: [
     BrowserModule
