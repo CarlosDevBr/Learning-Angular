79b5a03b7960d40d549402081774350f8713efaf
CarlosDevBr <cahzerohenrique@gmail.com>
2022-03-22 09:46:56 -0300

    7.I: Layout Structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { MeuComponenteComponent } from './meu-componente/meu-componente.component';
-import { MeuComponente2Component } from './meu-componente2/meu-componente2.component';
 import { FormsModule } from '@angular/forms';
 import { CursosComponent } from './cursos/cursos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MeuComponenteComponent,
-    MeuComponente2Component,
     CursosComponent
   ],
   imports: [
