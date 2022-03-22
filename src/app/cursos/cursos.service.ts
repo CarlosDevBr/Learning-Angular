74b621a9a3d3e5808bfd9708282e4ffd9e742760
CarlosDevBr <cahzerohenrique@gmail.com>
2022-03-22 08:47:50 -0300

    Base dos Services

--- src/app/cursos/cursos.service.ts
+++ src/app/cursos/cursos.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
 
+//Acesso total do serviço
 @Injectable({
   providedIn: 'root'
 })
 export class CursosService {
 
   constructor() { }
+
+  //Vetor de cursos
+
+  //Cadastro de cursos
+
+  //Seleção de cursos
+
+  //Seleção de um curso específico
+
+  //Alteração de cursos
+
+  //Exclusão de cursos
 }
