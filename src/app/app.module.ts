a50e80bbb27e6264336d4f612456eca56729946f
CarlosDevBr <cahzerohenrique@gmail.com>
2022-03-22 15:57:38 -0300

    7.L: added Register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
 import { CursosComponent } from './cursos/cursos.component';
+
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -11,15 +11,22 @@ export class CursosComponent implements OnInit {
 
   //Atributos
   public vetorCursos:Curso[] | undefined;
-  private curso:Curso | undefined;
-  private id: number | undefined;
+  public curso:Curso | undefined;
+  public id: number | undefined;
 
   //Construtor
   constructor(private servico:CursosService) { }
 
   //Inicialização
   ngOnInit() {
+    this.curso = new Curso("teste", 404, "teste");
     this.vetorCursos = this.servico.listar();
+  }
+
+  //Cadastrar
+  cadastrar(){
+    this.servico.cadastrar(this.curso);
+    this.curso = new Curso();
   }
 
 }
